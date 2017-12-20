f1736a94ec9b636365b4f960c5d0224c6c528e23
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-12-20 17:08:49 -0200

    Realizando testes com auto-refresh token

--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
-});
+});*/
 
 ApiRoute::version('v1', function(){
 	ApiRoute::group(['namespace' => 'CodeFlix\Http\Controllers\Api', 'as' => 'api'], function(){
 		ApiRoute::post('/access_token', [
-		    'uses' => 'AuthController@accessToken',
-            'middleware' => 'api.throttle',
-            'limit' => 10,
-            'expires' => 1
-        ])->name('.access_token');
+          'uses' => 'AuthController@accessToken',
+          'middleware' => 'api.throttle',
+          'limit' => 10,
+          'expires' => 1
+      ])->name('.access_token');
 
         ApiRoute::post('/refresh_token', [
             'uses' => 'AuthController@refreshToken',
@@ -33,15 +33,19 @@ ApiRoute::version('v1', function(){
             'expires' => 1
         ])->name('.refresh_token');
 
-		ApiRoute::group([
-		    'middleware' => ['api.throttle', 'api.auth'],
-            'limit' => 100,
-            'expires' => 3
-        ], function(){
-		    ApiRoute::post('/logout', 'AuthController@logout');
-            ApiRoute::get('/test', function(){
-               return "Aeee!!!";
-            });
+        ApiRoute::group([
+          'middleware' => ['api.throttle', 'api.auth'],
+          'limit' => 100,
+          'expires' => 3
+      ], function(){
+          ApiRoute::post('/logout', 'AuthController@logout');
+          ApiRoute::get('/test', function(){
+           return "Aeee!!!";
+       });
+
+          ApiRoute::get('/user', function(Request $request){
+            return $request->user('api');
         });
-	});
+      });
+    });
 });
--- tests/Feature/Api/LoginTest.php
+++ tests/Feature/Api/LoginTest.php
@@ -7,10 +7,12 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tymon\JWTAuth\JWTGuard;
+use Tymon\JWTAuth\JWT;
 
 class LoginTest extends TestCase
 {
-    use DatabaseMigrations;
+	use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -18,13 +20,68 @@ class LoginTest extends TestCase
      */
     public function testAccessToken()
     {
-        $urlGenerator = app(UrlGenerator::class)->version('v1');
+    	$this->makeJWTToken()
+    	->assertStatus(200)
+    	->assertJsonStructure(['token']);
+    }
 
-        $this->post($urlGenerator->route('api.access_token'), [
-            'email' => 'admin@user.com',
-            'password' => '123456'
-        ])
-        ->assertStatus(200)
-        ->assertJsonStructure(['token']);
+    public function testNotAuthorizedAccessApi()
+    {
+    	$this->get('api/user')
+    	->assertStatus(500);
+    }
+
+    public function testRefreshToken()
+    {
+    	$testResponse = $this->makeJWTToken();
+    	$token = $testResponse->json()['token'];
+
+    	sleep(61);
+
+    	$this->clearAuth();
+
+    	$testResponse = $this->get('api/user', [
+    		'Authorization' => "Bearer $token"
+    	])
+    	->assertJsonStructure(['user' => ['name']]);
+
+    	$headers = $testResponse->baseResponse->headers;
+    	$tokenResponse = $headers->get('Authorization');
+
+    	$this->assertNotEquals("Bearer $token", $tokenResponse);
+
+    	sleep(31);
+
+    	$this->clearAuth();
+
+    	$this->get('api/user', [
+    		'Authorization' => "Bearer $token"
+    	])->assertStatus(500);
+
+    }
+
+    protected function clearAuth()
+    {
+    	$reflectionClass = new \ReflectionClass(JWTGuard::class);
+
+    	$reflectionProperty = $reflectionClass->getProperty('user');
+    	$reflectionProperty->setAccessible(true);
+    	$reflectionProperty->setValue(\Auth::guard('api'), null);
+
+    	$jwt = app(JWT::class);
+    	$jwt->unsetToken();
+
+    	$dingoAuth = app(\Dingo\Api\Auth\Auth::class);
+    	$dingoAuth->setUser(null);
+    }
+
+    protected function makeJWTToken()
+    {
+    	$urlGenerator = app(UrlGenerator::class)->version('v1');
+
+    	return $this->post($urlGenerator->route('api.access_token'), [
+    		'email' => 'admin@user.com',
+    		'password' => '123456'
+    	]);
     }
 }
