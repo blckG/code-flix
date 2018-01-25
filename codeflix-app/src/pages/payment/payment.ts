23917aa6a0722dcfc4ac9968223c6a5e9552a0aa
Yuri Calabrez <yuri.calabrez@gmail.com>
2018-01-25 16:57:29 -0200

    Montando estrutura final de pagamento

--- codeflix-app/src/pages/payment/payment.ts
+++ codeflix-app/src/pages/payment/payment.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import scriptjs from 'scriptjs';
 import { UserResource } from '../../providers/resources/user.resource';
 import { PaymentResource } from '../../providers/resources/payment.resource';
+import { Subject } from 'rxjs/Subject';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 declare var PAYPAL;
 
@@ -24,10 +26,13 @@ export class PaymentPage {
   planId = null;
   ppplusLoaded = false;
   ppp = null;
+  subject = new Subject;
+  loading = null;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
+    public loadingCtrl: LoadingController,
     public userResource: UserResource,
     public paymentResource: PaymentResource
   ) {
@@ -35,17 +40,46 @@ export class PaymentPage {
   }
 
   ionViewDidLoad() {
-   scriptjs('https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js', () => {
-      
-   });
-   this.userResource.get().subscribe(user => this.user = user);
-   this.paymentResource.get(this.planId).subscribe(payment => this.payment = payment);
+    this.loading = this.loadingCtrl.create({
+      content: 'Carregando'
+    });
+    this.loading.present();
+
+    this.subject.subscribe(() => {
+      this.makePayPalPlus();
+    }, () => {
+      this.loading.dismiss();
+    });
+
+    scriptjs('https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js', () => {
+      if(typeof PAYPAL !== undefined){
+        this.ppplusLoaded = true;
+        this.subject.next();
+      } else {
+        this.subject.error('PayPal Plus not loaded');
+      }
+    });
+
+    this.userResource
+      .get()
+      .subscribe(user => {
+        this.user = user
+        this.subject.next();
+      }, () => this.subject.error('User not loaded'));
+
+    this.paymentResource
+      .get(this.planId)
+      .subscribe(payment => {
+        this.payment = payment;
+        this.subject.next();
+      }, () => this.subject.error('Payment not loaded'));
   }
 
-  makePayPalPlus(){
-    if(this.ppplusLoaded && this.payment !== null && this.user !== null){
+  makePayPalPlus() {
+    if (this.ppplusLoaded && this.payment !== null && this.user !== null) {
+      this.loading.dismiss();
       this.ppp = PAYPAL.apps.PPP({
-        approvalUrl: this.payment.approvalUrl,
+        approvalUrl: this.payment.approval_url,
         placeholder: 'ppplus',
         mode: 'sandbox',
         country: 'BR',
